724cddb0f40c8bc311e51869e9329e559d295952
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-09 17:39:45 +0200

    Added functionality to place ships

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,5 @@
 class Player
-    attr_accessor :my_board
-    attr_accessor :opponents_board
+    attr_accessor :my_board, :opponents_board, :ships
 
   def initialize
     @my_board = Grid.new
@@ -11,13 +10,35 @@ class Player
     # s1 = Ship.new("B")
   end
 
-  def place_ship(coord)
-    if @my_board.grid[coord] == "w"
-       @my_board.grid[coord] = "s"
-    else
-      'cant do that.'
+
+
+  def place_ships
+    @ships.each do |s|
+      place(s)
     end
   end
+
+  def place(s)
+    puts 'Give me your coordinates'
+    coord = gets.chomp
+    puts 'Choose direction H or V'
+    direction = gets.chomp
+      if can_place?(coord, direction, s.size)
+       @my_board.grid[coord.to_sym] = "s"
+       @opponents_board.grid[coord.to_sym] = "s"
+        puts 'Ship placed!'
+
+    else
+      puts 'Cant place boat here, try again.'
+      place(s)
+      end
+  end
+
+  def can_place? (coord, direction, size)
+    true
+  end
+
+
 
   def shoot_at(player,coord)
     oppboard = (player).my_board
