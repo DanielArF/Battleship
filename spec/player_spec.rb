7626a009d40b7321aa43c572d0976d8dcccd4e85
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-09 12:24:40 +0200

    fixed failing tests on player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,20 +17,19 @@ describe 'Player' do
 
   it 'can receive a hit' do
     subject.place_ship(:a1)
-    subject.shoot_at(opponent, :a1)
+    opponent.shoot_at(subject,:a1)
     expect(subject.my_board.grid).to include :a1 => "hit"
   end
 
   it 'can receive a miss' do
     subject.place_ship(:a2)
-    subject.shoot_at(:a1)
+    opponent.shoot_at(subject,:a1)
     expect(subject.my_board.grid).to include :a1 => "miss"
   end
 
   it 'can shoot at opponent' do
-  	player2 = Player.new
-  	subject.shoot(player2,:a1)
+  	subject.shoot_at(opponent,:a1)
   	expect(subject.opponents_board.grid).to include :a1 => "miss"
-  end 
+  end
 
 end
